7b3000fda51b3b1e5618e26041b4bfeedfca10e4
tchartchke <chartai.kl@gmail.com>
2021-04-04 23:43:36 -0500

    done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const ul = document.querySelector("ul#tasks")
+
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    item = `${document.querySelector("input#new-task-description").value} <button>x</button>`
+    const li = document.createElement('li')
+    li.innerHTML = item
+    ul.appendChild(li)
+    document.querySelector("input#new-task-description").value = ""
+
+}, false);
+
+
+ul.addEventListener("click", (event) => {
+  if (event.target.nodeName === "BUTTON") {
+    const li = event.target.parentNode;
+    li.remove()
+  }
+});
+
+
 });
