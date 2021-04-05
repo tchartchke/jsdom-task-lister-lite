12dbbc0b7aefe3a6b45664aab565eff608edb344
tchartchke <chartai.kl@gmail.com>
2021-04-05 10:21:05 -0500

    added priority and color

--- src/index.js
+++ src/index.js
@@ -4,21 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
 
+    const priorityColor = document.querySelector("select#priority").value
     item = `${document.querySelector("input#new-task-description").value} <button>x</button>`
     const li = document.createElement('li')
     li.innerHTML = item
+    li.style.color = `#${priorityColor}`
     ul.appendChild(li)
     document.querySelector("input#new-task-description").value = ""
 
-}, false);
+  }, false);
 
-
-ul.addEventListener("click", (event) => {
-  if (event.target.nodeName === "BUTTON") {
-    const li = event.target.parentNode;
-    li.remove()
-  }
-});
+  ul.addEventListener("click", (event) => {
+    if (event.target.nodeName === "BUTTON") {
+      const li = event.target.parentNode;
+      li.remove()
+    }
+  });
 
 
 });
